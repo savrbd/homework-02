be9a6574d271bf16cbcf6f6b22dc31592725532e
savr b <savr.bd@yandex.ru>
2021-09-01 07:23:43 +0300

    start task

---
+++ src/app.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Counters from "./components/counters";
+import NavBar from "./components/navBar";
+
+function App() {
+  const initialState = [
+    { value: 0, id: 1, name: "Ложка" },
+    { value: 4, id: 2, name: "Вилка" },
+    { value: 0, id: 3, name: "Тарелка" },
+    { value: 0, id: 4, name: "Стартовый набор минималиста" },
+    { value: 0, id: 5, name: "Ненужные вещи" },
+  ];
+  const [counters, setCounters] = useState(initialState);
+  const handleDelete = (counterId) => {
+    const newCounters = counters.filter((c) => c.id !== counterId);
+    setCounters(newCounters);
+  };
+  const handleDecrenent = (counterId) => {
+    const newCounters = [...counters];
+    const counterIndex = newCounters.findIndex(
+      (element) => element.id === counterId
+    );
+    newCounters[counterIndex].value--;
+    setCounters(newCounters);
+  };
+  const handleIncrement = (counterId) => {
+    const newCounters = [...counters];
+    const counterIndex = newCounters.findIndex(
+      (element) => element.id === counterId
+    );
+    newCounters[counterIndex].value++;
+    setCounters(newCounters);
+  };
+  const handleReset = () => setCounters(initialState);
+  return (
+    <div className="col-lg-8 mx-auto p-3 py-md-5">
+      <main>
+        <NavBar totalItems={counters.reduce((a, c) => a + c.value, 0)} />
+        <Counters
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrenent}
+          onReset={handleReset}
+          onDelete={handleDelete}
+          counters={counters}
+        />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/counter.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Counter = ({ id, value, name, onIncrement, onDecrement, onDelete }) => {
+  const formValue = () => {
+    return value === 0 ? "Ноль" : value;
+  };
+  const getBageClasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += value === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  return (
+    <div>
+      <h4>{name}</h4>
+      <span className={getBageClasses()}>{formValue()}</span>
+      <button
+        onClick={() => onIncrement(id)}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => onDecrement(id)}
+        className="btn btn-secondary btn-sm"
+      >
+        Decrement
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => onDelete(id)}
+      >
+        {" "}
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
